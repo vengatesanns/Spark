2ccd2d95882d6340a81e1346d3577bd6e941f6ae
vengatesanns <vengat19amd@gmail.com>
2021-03-07 09:24:44 -0800

    Customer Order and Datasets

--- build.sbt
+++ build.sbt
@@ -8,3 +8,7 @@ ThisBuild / useCoursier := false
 
 // https://mvnrepository.com/artifact/org.apache.spark/spark-core
 libraryDependencies += "org.apache.spark" %% "spark-core" % "3.1.0"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.1.1"
+
---
+++ src/main/scala/com/hackprotech/TotalAmountSpentByCustomer.scala
@@ -0,0 +1,44 @@
+package com.hackprotech
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+
+/**
+ * Total Amount Spend by the Customer
+ */
+object TotalAmountSpentByCustomer extends Serializable {
+
+
+  def splitCustomerDetails(line: String) = {
+    val rowData = line.split(",")
+    (rowData(0).toInt, rowData(2).toFloat)
+  }
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    // Spark Context
+    val sc = new SparkContext("local[*]", "Total_Amount_SpentBy_Customer")
+
+    // Read the Csv File
+    val customerDetails = sc.textFile("src/test/resources/customer-orders.csv")
+
+    //  val extractedCustomerDetails = customerDetails.map(x => {
+    //    val line = x.split(",")
+    //    (line(0).toInt, line(2).toFloat)
+    //  })
+
+    val extractedCustomerDetails = customerDetails.map(splitCustomerDetails)
+
+
+    // Reduce
+    val totalSpentCostByCustomer = extractedCustomerDetails.reduceByKey((x, y) => x + y)
+
+    // Swap Key and Values to do sorting
+    val results = totalSpentCostByCustomer.map(x => (x._2, x._1)).sortByKey(ascending = false).collect()
+
+
+    results.foreach(println)
+  }
+
+
+}
